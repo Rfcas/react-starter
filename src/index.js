d5bd6c876a792f55219b1b7cb25e1654223c7dd1
Rafael Castro <rafael.castro@thought-creator.com>
2018-04-05 09:46:08 +0100

    Hello world

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import './style.sass';
+
+const App = () => (
+    <div>
+        <h1>Hello, world!</h1>
+        {/* <h2>Os 11 porcos do José!</h2>
+        <img src="http://agriculturaemar.com/wp-content/uploads/2016/01/porcos-001-e1477414680302-660x330.jpg"/> */}
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
