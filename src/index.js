19cbdfd3d4b1d9a68ef6ee7b4244e20092852229
Rafael Castro <rafael.castro@thought-creator.com>
2018-04-05 10:49:47 +0100

    Environments added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import env from '@env';
 
 // import './style.sass';
 
-const App = () => (
-    <div>
-        <h1>Hello, world!</h1>
-        {/* <h2>Os 11 porcos do José!</h2>
-        <img src="http://agriculturaemar.com/wp-content/uploads/2016/01/porcos-001-e1477414680302-660x330.jpg"/> */}
-    </div>
-);
+const App = () => {
+    console.log('env', env);
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            {/* <h2>Os 11 porcos do José!</h2>
+            <img src="http://agriculturaemar.com/wp-content/uploads/2016/01/porcos-001-e1477414680302-660x330.jpg"/> */}
+        </div>
+    )
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
